4c386313e159135d3af4d3f09efaf730ee53924c
jin <dillonhua@gmail.com>
2017-08-23 15:06:25 +0800

    use 20s to test stability

--- main/cap.c
+++ main/cap.c
@@ -22,7 +22,7 @@ void set_upload_flag(int a)
 {
 	printfLog(CAP_PROCESS"set upload flag\n");
 	g_upload=1;
-	alarm(60);
+	alarm(20);
 }
 //format sensor history data
 void set_upload_data(char *id,struct nano *history,long *cnt,char *data,char *date)
@@ -1942,7 +1942,7 @@ int cap_init()
 		write_data(ADDR_PM10_SHOW_PIC_PPM,0x01);
 		write_data(ADDR_HCHO_SHOW_PIC_PPM,0x01);
 		signal(SIGALRM, set_upload_flag);
-		alarm(60);
+		alarm(20);
 		while(1)
 			cap_data_handle();
 	}
