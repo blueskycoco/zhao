b98d19a4591083cfcdd4a135958aa51efaa38903
Your Name <you@example.com>
2018-05-10 02:14:30 +0800

    update log place

--- main/log.c
+++ main/log.c
@@ -12,7 +12,7 @@
 
 #define PATH_BUF_SIZE 	256
 #define LOG_LENGTH		1024
-#define LOG_PATH		"/mnt/cdrom/log"
+#define LOG_PATH		"/opt/log"
 char log_name[256]={0};
 pthread_mutex_t mutex_log;
 void printfLog(const char *fmt, ...)
